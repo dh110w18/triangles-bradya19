594638b754ad2dc9086ade305dbe5a5e6e2acddb
Audrey Brady <bradya19@mail.wlu.edu>
2018-03-01 21:46:44 -0500

    committ 2

---
+++ script.js
@@ -0,0 +1,34 @@
+/* Part 1: Area of a triangle */
+var base= 30;
+var height= 40;
+var area= (1/2)* base * height;
+var message1 = 'The area of a triangle with a base of ' + base + 'and a height of ' + height + 'is equal to ' + area;
+document.getElementById("body1").innerHTML = message1;
+
+/* Part 2: Hypotenuse of a triangle */
+function calcHypotenuse(b, h){
+    return Math.sqrt((b*b)+(h*h));
+};
+calcHypotenuse(30, 40);
+var message2= 'The hypotenuse of a triangle with a base of ' + b + 'and a height of ' + h + 'is equal to ' + calcHypotenuse;
+document.getElementById("body2").innerHTML = message2;
+
+/* Part 3: Triangle Object */
+var triangle = {
+    base= 30,
+    height= 40,
+    canvasId= canvas,
+    calcArea: function(){
+        return (1/2)*this.base*this.height;
+    },
+    calcHypotenuse: function(){
+        return Math.sqrt((this.base*this.base)+(this.height*this.height));
+    },
+    drawIt: function(){
+        drawTriangle(this.base, this.height, this.canvasId);
+    },
+    drawIt()
+}
+var message3= 'The area of a triangle with a base of ' + this.base + 'and a height of ' + this.height + 'is equal to ' + calcArea;
+var message4= 'The hypotenuse of a triangle with a base of ' + this.base + 'and a height of ' + this.height + 'is equal to ' + calcHypotenuse;
+document.getElementById("body3").innerHTML = 'The area of a triangle with a base of ' + this.base + 'and a height of ' + this.height + 'is equal to ' + calcArea + '\n' + message4;
