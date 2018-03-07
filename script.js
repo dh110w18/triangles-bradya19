f0e8752736b1951852b12454d2ec8d95a257b4fa
Audrey Brady <bradya19@mail.wlu.edu>
2018-03-07 16:14:33 -0500

    triangles2commit1

--- script.js
+++ script.js
@@ -32,3 +32,25 @@ var message3= 'The area of a triangle with a base of ' + triangle.base + ' and a
 var message4= 'The hypotenuse of a triangle with a base of ' + triangle.base + ' and a height of ' + triangle.height + ' is equal to ' + triangle.calcHypotenuse();
 document.getElementById("block3").innerHTML = 'The area of a triangle with a base of ' + triangle.base + ' and a height of ' + triangle.height + ' is equal to ' + triangle.calcArea() + '.'+ '\n' + message4 + '.';
 triangle.drawIt();
+
+//Part 4: User entered base and height
+var userBase= prompt("Enter a value for the triangle's base that is at least 10");
+if(userBase < 10){
+    alert("ERROR: Base must be greater than or equal to 10.");
+} else{
+    var userHeight= prompt("Enter a value for the triangle's height that is greater than 0");
+};
+if(userHeight <= 0){
+    alert("ERROR: Height must be greater than 0.");
+} else{
+    triangle.base = userBase;
+    triangle.height = userHeight;
+};
+var message5 = 'The area of a triangle with a base of ' + userBase + ' and a height of ' + userHeight + ' is equal to ' + triangle.calcArea();
+var message6= 'The hypotenuse of a triangle with a base of ' + userBase + ' and a height of ' + userHeight + ' is equal to ' + triangle.calcHypotenuse();
+document.getElementById("block4").innerHTML = 'The area of a triangle with a base of ' + userBase + ' and a height of ' + userHeight + ' is equal to ' + triangle.calcArea(); + '.'+ '\n' + message6 + '.';
+triangle.canvasId=canvas2;
+triangle.drawIt()
+;
+
+//Part 5: Lots of Triangles
