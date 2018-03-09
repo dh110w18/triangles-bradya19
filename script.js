6990c2e8dbb3431f6a645bf0a4273964c5820760
Audrey Brady <bradya19@mail.wlu.edu>
2018-03-08 23:36:49 -0500

    commitagain

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ if(userHeight <= 0){
 };
 var message5 = 'The area of a triangle with a base of ' + userBase + ' and a height of ' + userHeight + ' is equal to ' + triangle.calcArea();
 var message6= 'The hypotenuse of a triangle with a base of ' + userBase + ' and a height of ' + userHeight + ' is equal to ' + triangle.calcHypotenuse();
-document.getElementById("block4").innerHTML = 'The area of a triangle with a base of ' + userBase + ' and a height of ' + userHeight + ' is equal to ' + triangle.calcArea() + '.'+ '\n' + message6 + '.';
+document.getElementById("block4").innerHTML = 'The area of a triangle with a base of ' + userBase + ' and a height of ' + userHeight + ' is equal to ' + triangle.calcArea()+ '.'+ '\n' + message6 + '.';
 triangle.canvasId=canvas2;
 triangle.drawIt();
 
